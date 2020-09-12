d4c9868c73e08f177e5349904f71bf6da7520ed7
NP <navdeep.puri@gmail.com>
2020-09-12 14:08:02 -0400

    Added code to dynamically populate searched cities column

--- script.js
+++ script.js
@@ -1,8 +1,81 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-console.log("Document ready");
+    console.log("Document ready");
 
 
+    /**
+     * GLOBAL VARIABLES
+     * 
+     */
+    var citySearchedArray = [];
+
+
+    /**
+     * 
+     * DOM ELEMENTS
+     */
+
+    // City search button
+    var citySearchBtn = $("#city-search-button");
+    // City name input box
+    var cityNameInput = $("#city-name");
+    // city history table body
+    var cityHistoryTableBody = $("#city-history-table");
+
+
+
+
+    /**
+     * 
+     * FUNCTION DEFINITIONS
+     */
+
+    // Function to display already searched cities
+    function renderCityNames() {
+
+        cityHistoryTableBody.empty();
+        
+        for (var i = 0; i < citySearchedArray.length; i++) {
+            // add city name to the already searched city list
+
+            var cityRow = $("<tr>");
+            var cityCol = $("<td>");
+            cityCol.text(citySearchedArray[i]);
+            cityRow.append(cityCol);
+            cityHistoryTableBody.append(cityRow);
+        }
+    }
+
+    /***
+     * FUNCTION CALLS
+     */
+
+
+
+    /**
+     * EVENT HANDLERS
+     * 
+     */
+
+
+    // Event Handler for city search button
+    citySearchBtn.on("click", function (event) {
+
+        event.preventDefault();
+
+        console.log("City search button clicked");
+
+        // get the city name from input text box
+        var cityName = cityNameInput.val();
+        console.log(cityName);
+
+        // add the city to the array
+        if (citySearchedArray.indexOf(cityName) === -1)
+            citySearchedArray.push(cityName);
+        // render city names
+        renderCityNames();
+
+    });
 
 
 
